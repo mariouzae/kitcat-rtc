fa40179b53762fa6725ba0e0a34d764321f777c7
Mario Uzae <mario.uzae@ontop.inf.br>
2018-09-17 15:04:06 -0300

    Adding webrtc getLocaMedia and RTCPeerConnection functions

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,39 @@
 $(function () {
     const socket = io('http://localhost:3000/');
-})
+});
+
+const localVideo = document.querySelector('#localVideo');
+const startVideo = document.querySelector('#startVideo');
+var localMediaStream = null;
+var peerConnection = null;
+
+startVideo.addEventListener('click', () => {
+    getLocalMedia();
+});
+
+function getLocalMedia() {
+    const contraints = {
+        video: false,
+        audio: true
+    };
+    navigator.mediaDevices.getUserMedia(contraints)
+        .then(gotLocalMediaStream)
+        .catch();
+};
+
+async function gotLocalMediaStream(mediaStream) {
+    localVideo.srcObject = mediaStream;
+    localMediaStream = mediaStream;
+    //listLocalDevice(localMediaStream);
+    await createPeerConnection();
+};
+
+async function createPeerConnection() {
+    peerConnection = new RTCPeerConnection({
+        iceServers: [     // Information about ICE servers - Use your own!
+            {
+                urls: "stun:stun.stunprotocol.org"
+            }
+        ]
+    });
+}
